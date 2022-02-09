3ab1ea4df972e791fed78817b71f884c6831488d
SP7TECH <spandey247789@gmail.com>
2022-02-10 02:42:21 +0530

    Implemented lists

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,97 +1,105 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
+const lnks = [];
+
 const userSchema = {
-  name: String,
-  email: String,
-  password: String,
+    name: String,
+    email: String,
+    password: String,
+    links: lnks
 };
 
 const User = mongoose.model("User", userSchema);
 
 exports.getHome = (req, res, next) => {
-  res.render("index");
+    res.render("index");
 };
 
 exports.getLogin = (req, res, next) => {
-  res.render("login");
+    res.render("login");
 };
 
 exports.postLogin = (req, res, next) => {
-  var reqbody = req.body;
+    var reqbody = req.body;
 
-  User.findOne(
-    {
-      email: reqbody.email,
-    },
-    (err, result) => {
-      if (result) {
-        bcrypt.compare(reqbody.password, result.password, (fail, pass) => {
-          if (pass) {
-            req.session.userId = result._id;
-            req.session.email = result.email;
-            req.session.name = result.name;
-            res.redirect("/lists");
-          }
-        });
-      }
-    }
-  );
+    User.findOne({
+            email: reqbody.email,
+        },
+        (err, result) => {
+            if (result) {
+                bcrypt.compare(reqbody.password, result.password, (fail, pass) => {
+                    if (pass) {
+                        req.session.userId = result._id;
+                        req.session.email = result.email;
+                        req.session.name = result.name;
+                        res.redirect("/lists");
+                    }
+                });
+            }
+        }
+    );
 };
 
 exports.getSignUp = (req, res, next) => {
-  res.render("signup");
+    res.render("signup", {
+        errorMessage: "Register Yourself!"
+    });
 };
 
 exports.postSignUp = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: hash,
-    });
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hash,
+        });
 
-    newUser.save(function (err, result) {
-      if (err) {
-        if (err.code == 11000) {
-          res.render("signup", {
-            errorMessage: "Email Already Exists.",
-          });
-        }
-      } else {
-        res.redirect("/lists");
-      }
+        newUser.save(function (err, result) {
+            if (err) {
+                if (err.code == 11000) {
+                    res.render("signup", {
+                        errorMessage: "Email Already Exists.",
+                    });
+                }
+            } else {
+                res.redirect("/lists");
+            }
+        });
     });
-  });
-
-  console.log("working");
 };
 
 exports.getLogout = (req, res, next) => {
-  req.session.destroy((err) => {
-    if (err) {
-      console.log("Session detroy error.");
-    } else {
-      res.redirect("/");
-    }
-  });
+    req.session.destroy((err) => {
+        if (err) {
+            console.log("Session detroy error.");
+        } else {
+            res.redirect("/");
+        }
+    });
 };
 
-exports.postLink = async (req, res, next) => {
-  var id = req.session.id;
-  const lnks = [];
+exports.postLink = (req, res, next) => {
 
-  lnks.push(req.body.link);
+    lnks.push(req.body.link);
 
-  let doc = await User.findOneAndUpdate({ email: req.session.email }, { links: lnks }, (err, result) => {
-    if (result) {
-      console.log(result);
-    } else {
-      console.log("error--> ", err);
-    }
-  });
+    User.findOneAndUpdate({
+        email: req.session.email
+    }, {
+        links: lnks
+    }, (err, result) => {
+        if (result) {
+            console.log(lnks);
+        } else {
+            console.log("error--> ", err);
+        }
+    });
+
+    res.redirect("/lists");
 };
 
 exports.getLists = (req, res, next) => {
-  res.render("list");
+    res.render("list", {
+        links: lnks
+    });
 };
